49004414c9fe5f90b30d802dc8869a8e6785e1d4
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-13 23:37:58 +0800

    Initial commit

---
+++ src/main/java/com/kyddaniel/QuizAppQuizMicroservice/QuizAppQuizMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.QuizAppQuizMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppQuizMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppQuizMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyddaniel/QuizAppQuizMicroservice/QuizAppQuizMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.QuizAppQuizMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppQuizMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
